bd9c324815b2cb049ad8ed5367a8824ea9624ffb
mark279455 <mark279455@ntlworld.com>
2023-07-02 16:34:37 +0100

    Completed 'Django Recipe Sharing Tutorial - 15. Wrapping up the home page'

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import AddRecipe, Recipes, RecipeDetail
+from .views import AddRecipe, Recipes, RecipeDetail, DeleteRecipe, EditRecipe
 
 
 urlpatterns = [
     path("add/", AddRecipe.as_view(), name="add_recipe"),
-    path("recipes/", Recipes.as_view(), name="recipes"),
+    path("", Recipes.as_view(), name="recipes"),
     path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail"),
-    # path("delete/<slug:pk>/", DeleteRecipe.as_view(), name="delete_recipe"),
-    # path("edit/<slug:pk>/", EditRecipe.as_view(), name="edit_recipe",)
+    path("delete/<slug:pk>/", DeleteRecipe.as_view(), name="delete_recipe"),
+    path("edit/<slug:pk>/", EditRecipe.as_view(), name="edit_recipe",)
 ]
